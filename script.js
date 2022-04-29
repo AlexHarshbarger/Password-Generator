baea227368aaed248ebc6c63c3cc204d303718bd
Alex Harshbarger <harshbarger.a@gmail.com>
2022-04-29 19:42:52 -0400

    generic password generator

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+const generate = document.querySelector('#generate');
+const header = document.querySelector('.header');
 const range = document.querySelector('#ran');
-let length = 9;
+const numCheck = document.querySelector('.numcheck');
+const letLower = document.querySelector('.letlower');
+const letUpper = document.querySelector('.letupper');
+const specialChar = document.querySelector('.specialchar');
+const output = document.querySelector('.output');
+// const lowerStr = "abcdefghijklmnopqrstuvwxyz";
+// const upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const numberStr = "0123456789";
+// const specialStr = "!()*+,-./:;<=>?'@[]^_`{|}~"
 
-range.onchange = (e) => {
-    document.querySelector('label[for="size"]').textContent = "Length of Password:   "
-    document.querySelector('label[for="size"]').textContent += e.target.value;
-    return length = e.target.value;
+const characters = "!()*+,-./:;<=>?'@[]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+output.innerHTML = "Length of Password: " + 16;
+
+range.addEventListener('input', function () {
+    output.innerHTML = "Length of Password:  " + range.value;
+}, false);
+
+generate.onclick = () => {
+    let password = "";
+    for (let i = 0; i <= range.value - 1; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    header.innerHTML = password;
 }
+
