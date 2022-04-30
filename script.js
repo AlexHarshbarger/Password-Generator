1a9babc62fe1565e1f9456150ef2c5914422596a
Alex Harshbarger <harshbarger.a@gmail.com>
2022-04-30 10:51:01 -0400

    check boxes are working, password updates with correct string based on checked or not

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const letLower = document.querySelector('.letlower');
 const letUpper = document.querySelector('.letupper');
 const specialChar = document.querySelector('.specialchar');
 const output = document.querySelector('.output');
-// const lowerStr = "abcdefghijklmnopqrstuvwxyz";
-// const upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// const numberStr = "0123456789";
-// const specialStr = "!()*+,-./:;?'@[]^_`{|}~"
+const lowerStr = "abcdefghijklmnopqrstuvwxyz";
+const upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberStr = "0123456789";
+const specialStr = "!()*+,-./:;?'@[]^_`{|}~"
 
-const characters = "!()*+,-./:;?='@[]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let characters = "";
 output.innerHTML = "Length of Password: " + 16;
 
 range.addEventListener('input', function () {
@@ -20,6 +20,7 @@ range.addEventListener('input', function () {
 
 generate.onclick = () => {
     let password = "";
+    isChecked(numCheck, letLower, letUpper, specialChar)
     for (let i = 0; i <= range.value - 1; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
         console.log(password)
@@ -27,5 +28,37 @@ generate.onclick = () => {
     if (password.length < range.value) {return}
     header.innerHTML = password;
     console.log(password.length)
+}
+
+function isChecked(a, b, c, d) {
+    if (a.checked && !b.checked && !c.checked && !d.checked) {
+        return characters = numberStr;
+    } else if (!a.checked && b.checked && !c.checked && !d.checked) {
+        return characters = lowerStr;
+    } else if (!a.checked && !b.checked && c.checked && !d.checked) {
+        return characters = upperStr;
+    } else if (!a.checked && !b.checked && !c.checked && d.checked) {
+        return characters = specialStr;
+    } else if (a.checked && b.checked && !c.checked && !d.checked) {
+        return characters = numberStr + lowerStr;
+    } else if (a.checked && !b.checked && c.checked && !d.checked) {
+        return characters = numberStr + upperStr;
+    } else if (a.checked && !b.checked && !c.checked && d.checked) {
+        return characters = numberStr + specialStr;
+    } else if (!a.checked && b.checked && c.checked && !d.checked) {
+        return characters = lowerStr + upperStr;
+    } else if (!a.checked && b.checked && !c.checked && d.checked) {
+        return characters = lowerStr + specialStr;
+    } else if (!a.checked && !b.checked && c.checked && d.checked) {
+        return characters = upperStr + specialStr;
+    } else if (a.checked && b.checked && c.checked && !d.checked) {
+        return characters = numberStr + lowerStr + upperStr;
+    } else if (a.checked && b.checked && !c.checked && d.checked) {
+        return characters = numberStr + lowerStr + specialStr;
+    } else if (a.checked && !b.checked && c.checked && d.checked) {
+        return characters = numberStr + upperStr + specialStr;
+    } else if (!a.checked && b.checked && c.checked && d.checked) {
+        return characters = lowerStr + upperStr + specialStr;
+    }
 }
 
